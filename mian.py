335ba828778f5e6ddc8e2b4274a42ea451fe836c
xiaolingsaya <152492694+xiaolingsaya@users.noreply.github.com>
2023-12-22 00:29:49 +0800

    basic mode update
    basicly completed all required function. you can choose a style to draw between 2 provided styles.

--- mian.py
+++ mian.py
@@ -4,12 +4,101 @@ import numpy as np
 import time
 import sketch
 import binary
+from serial.tools import list_ports
+import pydobot
+import path
+
+def lining():
+    camera = Picamera2()
+    camera.still_configuration.main.size = (160,120)
+    camera.still_configuration.main.format = 'RGB888'
+    camera.configure("still")
+    camera.start ()
+    time.sleep(1)
+    def back(x):
+        pass
+    cv2.namedWindow('Output',256)
+    cv2.createTrackbar('shadow','Output' ,0,255, back)
+    cv2.createTrackbar('ksize','Output' ,0,250, back)
+    print("press s to save the image and q to quit")
+    print("you can still press s to save a new image than saved one")
+    while(True):
+        input_img = camera.capture_array("main")
+        cv2.imshow('ori',input_img)
+        gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+        inv_gray_img = 255 - gray_img
+        ksize=ksize=int(cv2.getTrackbarPos('ksize','Output')/5)
+        if ksize%2==0:
+            ksize+=1
+        sigma=0
+        blur_img = cv2.GaussianBlur(inv_gray_img, ksize=(ksize, ksize), sigmaX=sigma, sigmaY=sigma)
+        sketch_img = cv2.divide(gray_img, 255 - blur_img, scale=256)  
+        blurfinale = cv2.GaussianBlur(sketch_img, ksize=(7, 7), sigmaX=sigma, sigmaY=sigma)
+        ret,binary = cv2.threshold(blurfinale,235,255,cv2.THRESH_BINARY)
+        cv2.imshow('finale',binary)
+        # 使用Canny边缘检测提取边缘
+        edges = cv2.Canny(binary, 50, 150)
+        inv_edges = 255 - edges
+        cv2.imshow('Edges', inv_edges)
+        inputkey=cv2.waitKey(1)
+        if inputkey== ord('s'):
+            cv2.imwrite('/home/student/2100012869/finale/output.png',inv_edges)
+            finale=cv2.imread('/home/student/2100012869/finale/output.png')
+            cv2.imshow('result',finale)
+        if inputkey== ord('q'):
+            path.painting()
+            break
+    camera.stop()
+    cv2.destroyAllWindows()
+
+def binary():
+    camera = Picamera2()
+    camera.still_configuration .main.size = (160,120)
+    camera.still_configuration .main.format = 'RGB888'
+    camera.configure("still")
+    camera.start ()
+    time.sleep(1)
+    def back(x):
+        pass
+    cv2.namedWindow('Output',256)
+    cv2.createTrackbar('shadow','Output' ,0,255, back)
+    cv2.createTrackbar('ksize','Output' ,0,250, back)
+    print("press s to save the image and q to quit")
+    print("you can still press s to save a new image than saved one")
+    while(True):
+        input_img = camera.capture_array("main")
+        cv2.imshow('ori',input_img)
+        gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('gray',gray_img)
+        ksize=int(cv2.getTrackbarPos('ksize','Output')/5)
+        if ksize%2==0:
+            ksize+=1
+        sigma=0
+        blurry = cv2.GaussianBlur(gray_img, ksize=(ksize, ksize), sigmaX=sigma, sigmaY=sigma)
+        shadow=cv2.getTrackbarPos('shadow','Output')
+        ret,binary = cv2.threshold(blurry,shadow,255,cv2.THRESH_BINARY)
+        cv2.imshow('finale',binary)
+        edges = cv2.Canny(binary, 50, 150)
+        inv_edges = 255 - edges
+        cv2.imshow('Edges', inv_edges)
+        inputkey=cv2.waitKey(1)
+        if inputkey== ord('s'):
+            cv2.imwrite('/home/student/2100012869/finale/output.png',inv_edges)
+            finale=cv2.imread('/home/student/2100012869/finale/output.png')
+            cv2.imshow('result',finale)
+        if inputkey== ord('q'):
+            path.painting()
+            break
+    camera.stop()
+    cv2.destroyAllWindows()
 
 print("welcome to drawing station!")
 print("please choose the style you want:")
 print("1 for the lining,2 for the binary")
 print("you can check the sample to clarify your need")
-if cv2.waitKey(1)==ord('1'):
-    sketch.lining
-if cv2.waitKey(1)==ord('2'):
-    binary.binarydrawing
+if input()=="1":
+    lining()
+if input()=="2":
+    binary()
+
+
--- path.py
+++ path.py
@@ -1,20 +1,49 @@
 from serial.tools import list_ports
-
 import pydobot
+from picamera2 import Picamera2
+import cv2
+import numpy as np
+import time
 
-available_ports = list_ports.comports()
-print(f'available ports: {[x.device for x in available_ports]}')
-port = available_ports[0].device
-
-device = pydobot.Dobot(port=port, verbose=True)
-
-(x, y, z, r, j1, j2, j3, j4) = device.pose()
-print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
-
-for i in range(2):
-    device.move_to(x + 20, y, z, r, wait=True)
-    device.wait(1000)
-    device.move_to(x, y, z, r, wait=True)
-    device.wait(1000)
-
-device.close()
+def painting():
+    available_ports = list_ports.comports()
+    print(f'available ports: {[x.device for x in available_ports]}')
+    port = available_ports[0].device
+    device = pydobot.Dobot(port=port, verbose=True)
+    (x, y, z, r, j1, j2, j3, j4) = device.pose()
+    print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
+    x0=-60
+    y0=140
+    edges=cv2.imread('/home/student/2100012869/finale/output.png')
+    edges=255-edges
+    edges= cv2.Canny(edges,50,150)
+    # 找到轮廓并提取路径点
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    print("start drawing!")
+    # 对每个轮廓进行处理
+    i=0
+    for contour in contours:
+        # 逐点移动机器人
+        i+=1
+        for point in contour:
+            y, x = point[0]
+            # 假设这里设定机器人的移动参数
+            device.move_to(x+x0, y+y0, z, r, wait=True)  # 移动到轮廓上的点
+            
+        # 当一个轮廓结束时，抬起（增加z）
+        z += 30
+        device.move_to(x+x0, y+y0, z, r, wait=True)
+        device.wait(50)  # 等待一段时间
+        
+        # 下一个轮廓开始时放下（减少z）
+        z -= 30
+        device.move_to(x+x0, y+y0, z, r, wait=True)
+        
+    # 结束后清理
+    print(i)
+    device.move_to(x0, y0, z+30, r, wait=True)  # 移动到结束点
+    device.wait(500)  # 等待一段时间
+    print("finished!")
+    device.close()
+    
+painting()
